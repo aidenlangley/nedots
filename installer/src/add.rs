c4a759b53020476ccac51f99630980477c70d072
nedia <aiden@nedia.dev>
2022-04-15 16:17:23 +1200

    feat: More tests

--- installer/src/add.rs
+++ installer/src/add.rs
@@ -1,9 +1,9 @@
+use crate::utils::CopyOperation;
 use std::{
     fmt::Display,
     path::{Path, PathBuf},
+    process::{Command, Output},
 };
-
-use crate::utils::CopyOperation;
 
 pub(crate) enum AddError {
     Conflicts { path: String },
@@ -34,9 +34,64 @@ pub(crate) fn add_changes(
     }
 }
 
-pub(crate) fn git_add(repo_path: &Path) -> Result<(), AddError> {
-    todo!()
+pub(crate) fn git_add(parent_path: &Path) -> Result<Output, AddError> {
+    let output = if cfg!(target_os = "windows") {
+        todo!()
+    } else {
+        Command::new("git")
+            .args([
+                "-C",
+                parent_path
+                    .join(".nedots")
+                    .as_path()
+                    .to_string_lossy()
+                    .to_string()
+                    .as_str(),
+                "add",
+                ".",
+            ])
+            .output()
+            .expect("failed to execute process")
+    };
+
+    Ok(output)
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::settings::Settings;
+    use std::process::{Command, Output};
+
+    fn git_stash(path: &str) -> Output {
+        Command::new("git")
+            .args(["-C", path, "stash"])
+            .output()
+            .expect(&format!("Failed to stash `git` tree at {}!", path))
+    }
+
+    fn git_restore(path: &str) -> Output {
+        Command::new("git")
+            .args(["-C", path, "stash", "pop"])
+            .output()
+            .expect(&format!(
+                "Failed to restore `git` tree at {} to it's original state!",
+                path
+            ))
+    }
+
+    #[test]
+    fn git_add() {
+        let settings: Settings = crate::read_settings();
+        let repo_path = settings.path.join(".nedots");
+
+        git_stash(repo_path.to_string_lossy().to_string().as_str());
+
+        let git_add = super::git_add(&settings.path);
+        match git_add {
+            Ok(o) => assert!(o.status.success()),
+            Err(e) => panic!("{}", e),
+        }
+
+        git_restore(repo_path.to_string_lossy().to_string().as_str());
+    }
+}
--- installer/src/sanity_checks.rs
+++ installer/src/sanity_checks.rs
@@ -1,3 +1,32 @@
 pub(crate) fn check_git() {}
 pub(crate) fn check_flatpak() {}
 pub(crate) fn check_all() {}
+
+#[cfg(test)]
+mod tests {
+    use crate::{read_settings, settings::Settings};
+    use std::process::Command;
+
+    #[test]
+    fn git_status() {
+        let settings: Settings = read_settings();
+        let git_status = Command::new("git")
+            .args([
+                "-C",
+                settings
+                    .path
+                    .join(".nedots")
+                    .as_path()
+                    .to_string_lossy()
+                    .to_string()
+                    .as_str(),
+                "status",
+            ])
+            .output();
+
+        match git_status {
+            Ok(o) => assert!(o.status.success()),
+            Err(e) => panic!("{}", e),
+        }
+    }
+}
